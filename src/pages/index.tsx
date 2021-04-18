18a7938286e9d92ef0fddec4731fdf7fb4a22fba
ViniciusPassoni97 <viniciuspassoni97@gmail.com>
2021-04-17 23:00:43 -0300

    Mode head in index

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
 import Head from 'next/head'
 import styles from '../styles/home.module.scss';
 
+
 export default function Home() {
   return (
-    <div className={styles.title}>
+    <>
+      <Head>
+         <title>ig.news</title>
+      </Head>
       <h1>Ola mundo</h1>
-    </div>
+    </>
   )
 }
