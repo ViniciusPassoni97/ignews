0f111bdeaee747444f2b93b10a3e9900969fcf36
ViniciusPassoni97 <viniciuspassoni97@gmail.com>
2021-04-17 22:35:33 -0300

    Adding sass in  project

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,8 +1,9 @@
 import Head from 'next/head'
+import styles from '../styles/home.module.scss';
 
 export default function Home() {
   return (
-    <div>
+    <div className={styles.title}>
       <h1>Ola mundo</h1>
     </div>
   )
