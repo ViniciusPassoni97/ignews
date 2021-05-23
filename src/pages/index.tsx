f03ec03b569d3420298c47158772bf91fa0a8e67
ViniciusPassoni97 <viniciuspassoni97@gmail.com>
2021-05-23 02:11:57 -0300

    Home style

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,12 +1,25 @@
 import Head from 'next/head'
+import styles from './home.module.scss';
 
 export default function Home() {
   return (
     <>
       <Head>
-         <title>ig.news</title>
+         <title>Home | ig.news</title>
       </Head>
-      <h1>Ola mundo</h1>
+      <main className={styles.contentContainer}>
+        <section className={styles.hero}>
+            <span>
+               Hey, welcome
+            </span>
+            <h1>News about the <span>React </span>world</h1>
+            <p>
+              Get acess te all the publications <br/>
+              <span>for $9.93 month</span>
+            </p>
+        </section>
+        <img src="/images/avatar.svg" alt="gril coding" />
+      </main>
     </>
   )
 }
